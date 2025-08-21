bd13f59763f2781e7904ed867c7465b7af561271
joseah <alquicirajose@gmail.com>
2025-08-20 21:38:42 -0400

    adds missing exports

--- src/Dynema.jl
+++ src/Dynema.jl
@@ -17,7 +17,7 @@ using ProgressMeter
 
 export map_locus
 export adjust_phenotype
-export f, term, ncells, ndonors, stats, B, bootdist
+export f, fterm, ncells, ndonors, stats, bstat, coefs, snps, B, bootdist
 
 
 include("adjust_phenotype.jl")
