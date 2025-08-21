70adb5715628afc036075ac906cb6ac6ce34123e
joseah <alquicirajose@gmail.com>
2025-08-20 21:02:58 -0400

    adds accessors, renames struct fields, sorts summary stats when printing

--- src/Dynema_struct.jl
+++ src/Dynema_struct.jl
@@ -1,57 +1,133 @@
+# ---------------------------------------------------------------------------- #
+#                           Define DynemaModel struct                          #
+# ---------------------------------------------------------------------------- #
+
 struct DynemaModel
 
     f::FormulaTerm
     term::String
     ncells::Int
     ndonors::Int
-    summ_stats::DataFrame
+    stats::DataFrame
     B::Vector{Int64}
     boot_dist::AbstractVector
 
 end
 
+# ---------------------------------------------------------------------------- #
+#                               Define accessors                               #
+# ---------------------------------------------------------------------------- #
+
+"""
+
+`f(::Dynema.DynemaModel)`
+
+Extract formula used for a DynemaModel
+"""
+
+f(m::DynemaModel) = m.f
+
+"""
+
+`term(::Dynema.DynemaModel)`
+
+Extract formula used for a DynemaModel
+"""
+
+term(m::DynemaModel) = m.term
+
+"""
+
+`ncells(::Dynema.DynemaModel)`
+
+Extract number of cells used for a DynemaModel
+"""
+
+ncells(m::DynemaModel) = m.ncells
+
+"""
+
+`ndonors(::DynemaModel)`
+
+Extract number of donors/individuals for a DynemaModel
+"""
+
+ndonors(m::DynemaModel) = m.ndonors
+
+"""
+
+`stats(::Dynema.DynemaModel)`
+
+Extract summary statistics for a DynemaModel
+"""
+
+stats(m::DynemaModel) = m.stats
+
+"""
+
+`B(::Dynema.DynemaModel)`
+
+Extract number of bootstrap iterations applied iteratively 
+for a DynemaModel
+"""
+
+B(m::DynemaModel) = m.B
+
+
+"""
+
+`bootdist(::Dynema.DynemaModel)`
+
+Extract bootstrap stat distributions for each SNP for a DynemaModel
+"""
+
+bootdist(m::DynemaModel) = m.bootdist
+
+
+# ---------------------------------------------------------------------------- #
+#                                Define printing                               #
+# ---------------------------------------------------------------------------- #
 
 function Base.show(io::IO, ::MIME"text/plain", m::DynemaModel)
 
     print(Crayon(foreground = :light_yellow, bold = true), "\nDynamic non-parametric eQTL mapping (Dynema) model\n\n")
     print(Crayon(foreground = :green), "Wild cluster bootstrap via WildBootTests.jl\n\n")
-    print(Crayon(foreground = :blue), m.f, "\n\n")
+    print(Crayon(foreground = :blue), f(m), "\n\n")
 
 
     print(Crayon(reset = true, bold = true), "Term tested   = ")
-    println(Crayon(foreground = :red, bold = true), m.term)
+    println(Crayon(foreground = :red, bold = true), term(m))
 
     
     print(Crayon(reset = true, bold = true), "N. bootstraps = ")
-    println(Crayon(foreground = :red, bold = true), "$(sum(m.B))")
+    println(Crayon(foreground = :red, bold = true), "$(sum(B(m)))")
 
     
     print(Crayon(reset = true, bold = true), "N. SNPs       = ")
-    println(Crayon(foreground = :red, bold = true), "$(nrow(m.summ_stats))")
+    println(Crayon(foreground = :red, bold = true), "$(nrow(stats(m)))")
 
     
     print(Crayon(reset = true, bold = true), "N. cells      = ")
-    println(Crayon(foreground = :red, bold = true), "$(m.ncells)")
+    println(Crayon(foreground = :red, bold = true), "$(ncells(m))")
     
     print(Crayon(reset = true, bold = true), "N. donors     = ")
-    println(Crayon(foreground = :red, bold = true), "$(m.ndonors)")
+    println(Crayon(foreground = :red, bold = true), "$(ndonors(m))")
 
 
-    if nrow(m.summ_stats) >= 6
-        glance = first(m.summ_stats, 3)
-        push!(glance, fill("...", ncol(m.summ_stats)), promote = true)
-        glance = vcat(glance, last(m.summ_stats, 3))
+    if nrow(m.summ_stats) >= 10
         
+        glance = first(sort(stats(m), [order(:p), order(:stat, by = abs, rev = true)]), 10)
+        push!(glance, fill("...", ncol(stats(m))), promote = true)
 
     else
-        glance = m.summ_stats
+
+        glance = stats(m)
+
     end
-
-
 
     println(Crayon(reset = true), "\nResults")
     pretty_table(glance, header = (names(glance)))
-    println("** smallest p-value = $(2/sum(m.B)); report as p < $(2/sum(m.B))")
+    println("** smallest p-value = $(2/sum(B(m))); report as p < $(2/sum(B(m)))")
     
     
 end
